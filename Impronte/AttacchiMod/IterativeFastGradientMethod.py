75881925be4c1164154e5d0acffda28a8c31a6cf
umbertogagl97 <u.gagliardini@studenti.unina.it>
2021-11-10 15:08:38 +0100

    Update IterativeFastGradientMethod.py

--- Impronte/AttacchiMod/IterativeFastGradientMethod.py
+++ Impronte/AttacchiMod/IterativeFastGradientMethod.py
@@ -16,12 +16,7 @@ from art.utils import (
     projection,
     check_and_transform_label_format,
 )
-from utils import (
-    compute_mask,
-    test_average,
-    transf_resize,
-    trans_norm
-)
+from utils import *
 
 if TYPE_CHECKING:
     from art.utils import CLASSIFIER_LOSS_GRADIENTS_TYPE
